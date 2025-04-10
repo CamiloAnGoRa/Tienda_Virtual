1b3b9678b8e550b5db6c577e18c3c4fc96ab7dda
CamiloAnGoRa <201641142+CamiloAnGoRa@users.noreply.github.com>
2025-04-10 00:44:20 -0500

    Tienda virtual, css, js, carrito.html, index.html, vistas completas, faltan imagenes para los productos, funcionalidad completa, simulacion de carrito de compras completado

---
+++ js/productos.js
@@ -0,0 +1,182 @@
+[
+    {
+        "id": "chaqueta-01",
+        "titulo": "Chaqueta 01",
+        "imagen": "./images/chaqueta_1.png",
+        "categoria": {
+            "nombre": "Chaquetas",
+            "id": "chaquetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "chaqueta-02",
+        "titulo": "Chaqueta 02",
+        "imagen": "./images/chaqueta_2.png",
+        "categoria": {
+            "nombre": "Chaquetas",
+            "id": "chaquetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "chaqueta-03",
+        "titulo": "Chaqueta 03",
+        "imagen": "./images/chaqueta_3.png",
+        "categoria": {
+            "nombre": "Chaquetas",
+            "id": "chaquetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "chaqueta-04",
+        "titulo": "Chaqueta 04",
+        "imagen": "./images/chaqueta_4.png",
+        "categoria": {
+            "nombre": "Chaquetas",
+            "id": "chaquetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "chaqueta-05",
+        "titulo": "Chaqueta 05",
+        "imagen": "./images/chaqueta_5.png",
+        "categoria": {
+            "nombre": "Chaquetas",
+            "id": "chaquetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-01",
+        "titulo": "Camiseta 01",
+        "imagen": "",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-02",
+        "titulo": "Camiseta 02",
+        "imagen": "",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-03",
+        "titulo": "Camiseta 03",
+        "imagen": "",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-04",
+        "titulo": "Camiseta 04",
+        "imagen": "",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-05",
+        "titulo": "Camiseta 05",
+        "imagen": "",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-06",
+        "titulo": "Camiseta 06",
+        "imagen": "",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-07",
+        "titulo": "Camiseta 07",
+        "imagen": "",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "camiseta-08",
+        "titulo": "Camiseta 08",
+        "imagen": "",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "pantalon-01",
+        "titulo": "Pantalón 01",
+        "imagen": "",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "pantalon-02",
+        "titulo": "Pantalón 02",
+        "imagen": "",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "pantalon-03",
+        "titulo": "Pantalón 03",
+        "imagen": "",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "pantalon-04",
+        "titulo": "Pantalón 04",
+        "imagen": "",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "pantalon-05",
+        "titulo": "Pantalón 05",
+        "imagen": "",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 1000
+    }
+]
